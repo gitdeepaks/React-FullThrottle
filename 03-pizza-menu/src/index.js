aea0a1a73e53090e5a498cd37ee99333c84a8d38
gitdeepaks <deepaksankhyan92@outlook.com>
2023-07-11 19:28:20 +0530

    new commit 🚀

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -57,7 +57,12 @@ function App() {
 }
 
 function Header() {
-  const style = { color: "red", fontSize: "48px", textTransform: "uppercase" };
+  const style = {
+    color: "red",
+    fontSize: "48px",
+    textTransform: "uppercase",
+    textAlign: "center",
+  };
 
   return <h1 style={style}>Fast React Pizza Co.</h1>;
 }
