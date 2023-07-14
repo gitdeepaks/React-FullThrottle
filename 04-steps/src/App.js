712a2739b01ed019178b2598c79db773f5e98907
gitdeepaks <deepaksankhyan92@outlook.com>
2023-07-14 21:07:05 +0530

    new commit 🚀

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 
 const messages = [
   "Learn React ⚛️",
@@ -7,25 +8,33 @@ const messages = [
 ];
 
 export default function App() {
-  const step = 1;
+  const [step, setStep] = useState(1); // called as hook
+
+  const [test, setTest] = useState({ name: "Deepak" });
 
   function handlePrev() {
-    alert("Previous");
+    if (step > 1) setStep(step - 1);
   }
+
   function handleNext() {
-    alert("Next");
+    if (step < 3) setStep(step + 1);
+    // test.name = "Jonny";
+    setTest({ name: "July" });
+
+    // step = step + 1;
   }
 
   return (
     <div className="steps">
       <div className="numbers">
-        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={step >= 3 ? "active" : ""}>3</div>
       </div>
 
       <p className="message">
         Step{step}:{messages[step - 1]}
+        {test.name}
       </p>
       <div className="buttons">
         <button
