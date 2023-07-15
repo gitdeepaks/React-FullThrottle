32eb6016dc2301f51bb6f405df0b0aaf29d7dd65
gitdeepaks <deepaksankhyan92@outlook.com>
2023-07-15 12:12:39 +0530

    new commit 🚀

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -15,20 +15,23 @@ export default function App() {
   const [test, setTest] = useState({ name: "Deepak" });
 
   function handlePrev() {
-    if (step > 1) setStep(step - 1);
+    if (step > 1) setStep((s) => s - 1);
   }
 
   function handleNext() {
-    if (step < 3) setStep(step + 1);
+    if (step < 3) {
+      setStep((s) => s + 1);
+      setStep((s) => s + 1);
+    }
     // test.name = "Jonny";
-    setTest({ name: "July" });
+    setTest({ name: "Deepak" });
 
     // step = step + 1;
   }
 
   return (
     <>
-      <button className="close" onClick={() => setIsOpen(!isOpen)}>
+      <button className="close" onClick={() => setIsOpen((is) => !is)}>
         &times;
       </button>
 
