69c328a23a59520532f8c879e20bf7894bdc3006
gitdeepaks <deepaksankhyan92@outlook.com>
2023-07-20 23:21:17 +0530

    new commit 🚀

--- 07-usepopcorn/src/index.js
+++ 07-usepopcorn/src/index.js
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
 
 import StarRating from "./StarRating";
 
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />;
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      messages={["terrible", "bad", "okey", "good", "Amazing"]}
+    />
+    <StarRating size={24} color="red" className="test" defaultRating={3} />
     {/* <StarRating maxRating={10} /> */}
+    <Test />
   </React.StrictMode>
 );
