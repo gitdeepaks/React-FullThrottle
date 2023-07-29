be82c81ef5c0ec0c640fe5d76ddc07b86e8a684d
gitdeepaks <deepaksankhyan92@outlook.com>
2023-07-29 16:01:24 +0530

    new commit 🚀

--- 07-usepopcorn/src/index.js
+++ 07-usepopcorn/src/index.js
@@ -2,24 +2,25 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import Appv2 from "./Appv2";
 
 import StarRating from "./StarRating";
 
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
+// function Test() {
+//   const [movieRating, setMovieRating] = useState(0);
 
-  return (
-    <div>
-      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />;
-      <p>This movie was rated {movieRating} stars</p>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />;
+//       <p>This movie was rated {movieRating} stars</p>
+//     </div>
+//   );
+// }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Appv2 />
     {/* <StarRating
       maxRating={"ss"}
       messages={["terrible", "bad", "okey", "good", "Amazing"]} */}
