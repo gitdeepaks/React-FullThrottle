4f6d1ed46e58d79a58ded9b039b4b05568d41a7c
gitdeepaks <deepaksankhyan92@outlook.com>
2023-07-30 14:45:42 +0530

    new commit 🚀

--- 09-classy-weather-app/src/App.js
+++ 09-classy-weather-app/src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 
 class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { count: 5 };
+  }
   render() {
     return (
       <div>
         <button>-</button>
-        <span>0</span>
+        <span>{this.state.count}</span>
         <button>+</button>
       </div>
     );
