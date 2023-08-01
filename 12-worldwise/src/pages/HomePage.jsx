0df942585d08906f86991c204d629fe1765d1f5e
gitdeepaks <deepaksankhyan92@outlook.com>
2023-08-01 21:53:12 +0530

    new Commit🚀

--- 12-worldwise/src/pages/HomePage.jsx
+++ 12-worldwise/src/pages/HomePage.jsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import PageNav from "../components/pageNav";
 
 function HomePage() {
@@ -6,7 +5,6 @@ function HomePage() {
     <div>
       <PageNav />
       <h1>WorldWise</h1>
-      <Link to="./pricing">Pricing</Link>
     </div>
   );
 }
