d4fdeda444d1f03f44a353c7bf4574ebccda4998
gitdeepaks <deepaksankhyan92@outlook.com>
2023-08-01 21:34:27 +0530

    new Commit🚀

--- 12-worldwise/src/App.jsx
+++ 12-worldwise/src/App.jsx
@@ -1,5 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./pages/Products";
+import Pricing from "./pages/Pricing";
+import HomePage from "./pages/HomePage";
 function App() {
-  return <div>WorldWine</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="product" element={<Product />}></Route>
+        <Route path="pricing" element={<Pricing />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
