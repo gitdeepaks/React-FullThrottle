808ecf6552bd5221f0d71220ea37f6c395dfe4e5
gitdeepaks <deepaksankhyan92@outlook.com>
2023-08-15 16:36:52 +0530

    new Commit🚀

---
+++ 16-fast-react-pizza/src/features/user/UserName.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function UserName() {
+  return <div className="hidden text-sm font-semibold md:block">Deepak</div>;
+}
+
+export default UserName;
--- 16-fast-react-pizza/src/ui/AppLayout.jsx
+++ 16-fast-react-pizza/src/ui/AppLayout.jsx
@@ -16,7 +16,7 @@ function AppLayout() {
       <Header />
 
       <main>
-        <h1>Content</h1>
+        {/* <h1>Content</h1> */}
         <Outlet />
       </main>
 
