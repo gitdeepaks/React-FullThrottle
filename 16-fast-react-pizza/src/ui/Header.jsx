ac2936ea292a90edc04ad307cf8930eeb24f3772
gitdeepaks <deepaksankhyan92@outlook.com>
2023-08-16 13:33:09 +0530

    new Commit🚀

--- 16-fast-react-pizza/src/ui/Header.jsx
+++ 16-fast-react-pizza/src/ui/Header.jsx
@@ -7,7 +7,7 @@ function Header() {
   return (
     <header className="flex items-center justify-between border-b border-stone-200 bg-yellow-500 px-4 py-3 uppercase sm:px-6">
       <Link to="/" className="tracking-widest">
-        Fasr React Pizza Co.
+        The Pizza Co.
       </Link>
 
       <SearchOrder />
