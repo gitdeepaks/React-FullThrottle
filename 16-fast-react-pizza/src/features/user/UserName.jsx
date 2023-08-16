aab8702a99ac431bdecc881afb79acefae78204a
gitdeepaks <deepaksankhyan92@outlook.com>
2023-08-16 17:10:28 +0530

    new Commit🚀

--- 16-fast-react-pizza/src/features/user/UserName.jsx
+++ 16-fast-react-pizza/src/features/user/UserName.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 function UserName() {
-  return <div className="hidden text-sm font-semibold md:block">Deepak</div>;
+  const username = useSelector((state) => state.user.username);
+
+  if (!username) return null;
+
+  return (
+    <div className="hidden text-sm font-semibold md:block">{username}</div>
+  );
 }
 
 export default UserName;
