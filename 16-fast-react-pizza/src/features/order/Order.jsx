2527822d79ca0918b6b2f715d08a4ae20aa26a45
gitdeepaks <deepaksankhyan92@outlook.com>
2023-08-17 10:43:45 +0530

    new Commit🚀

--- 16-fast-react-pizza/src/features/order/Order.jsx
+++ 16-fast-react-pizza/src/features/order/Order.jsx
@@ -1,6 +1,6 @@
 // Test ID: IIDSAT
 
-import { useLoaderData } from "react-router-dom";
+import { useFetcher, useLoaderData } from "react-router-dom";
 import { getOrder } from "../../services/apiRestaurant";
 import OrderItem from "./OrderItem";
 import {
@@ -8,9 +8,22 @@ import {
   formatCurrency,
   formatDate,
 } from "../../utils/helpers";
+import { useEffect } from "react";
 
 function Order() {
   const order = useLoaderData();
+
+  const fether = useFetcher();
+
+  useEffect(
+    function () {
+      if (!fether.data && fether.state === "idle") fether.load("/menu");
+    },
+    [fether],
+  );
+
+  console.log(fether.data);
+
   // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
   const {
     id,
@@ -57,7 +70,15 @@ function Order() {
 
       <ul className="divider divider-stone-200 divide-y border-b border-t">
         {cart.map((item) => (
-          <OrderItem item={item} key={item.pizzaId}></OrderItem>
+          <OrderItem
+            item={item}
+            key={item.pizzaId}
+            isLoadingIngredients={fether.state === "loading"}
+            ingredients={
+              fether.data?.find((el) => el.id === item.pizzaId)?.ingredients ??
+              []
+            }
+          ></OrderItem>
         ))}
       </ul>
 
