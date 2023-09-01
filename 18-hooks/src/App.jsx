64ebcab8b39576eb1c7f4ee213dcf7aad4934789
gitdeepaks <deepaksankhyan92@outlook.com>
2023-09-01 17:58:20 +0530

    new Commit🚀

--- 18-hooks/src/App.jsx
+++ 18-hooks/src/App.jsx
@@ -1,20 +1,38 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  let counter = 15;
+  const [counter, setCounter] = useState(15);
+  // let counter = 15;
 
   const addValue = () => {
-    console.log("clicked", counter);
-    counter = counter + 1;
+    // counter = counter + 1;
+    setCounter(counter + 1);
+    // console.log("clicked", setCounter);
+
+    if (counter === 20) {
+      return setCounter(20);
+    }
+  };
+
+  const removeValue = () => {
+    // console.log("clicked", counter);
+    // counter = counter + 1;
+    setCounter(counter - 1);
+
+    if (counter === 0) {
+      return setCounter(0);
+    }
   };
   return (
     <>
       <h1>Chai or Hooks</h1>
       <h2>Counter value:{counter}</h2>
 
-      <button onClick={addValue}>Add Value</button>
+      <button onClick={addValue}>Add Value {counter}</button>
       <br />
-      <button>Remove Value</button>
+      <button onClick={removeValue}>Remove Value {counter}</button>
+      <p>footer{counter}</p>
     </>
   );
 }
