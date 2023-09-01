48692bf56da97753c84f9024164f9285df7e621e
gitdeepaks <deepaksankhyan92@outlook.com>
2023-09-01 21:32:45 +0530

    new Commit🚀

--- 19-hookswithtailwind/src/App.jsx
+++ 19-hookswithtailwind/src/App.jsx
@@ -1,8 +1,14 @@
+import Card from "./components/Card";
+
 function App() {
   return (
-    <div>
-      <h1 className="text-6xl text-blue-600">Hello</h1>
-    </div>
+    <>
+      <h1 className=" justify-center items-center flex text-3xl text-blue-500 rounded-xl bg-green-400 p-4 mb-4">
+        Hello
+      </h1>
+      <Card username="chai or code" btnText="ClickMe" />
+      <Card username="Deepak" />
+    </>
   );
 }
 
