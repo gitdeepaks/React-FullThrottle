222dda71870eedb65667527adf4060631c66a5c5
gitdeepaks <deepaksankhyan92@outlook.com>
2023-09-19 21:37:28 +0530

    new Commit🚀

---
+++ 26-context-todos/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      height: {
+        "screen-1/2": "50vh",
+        "screen-1/3": "33.333333vh",
+        "screen-2/3": "66.666667vh",
+        "screen-1/4": "25vh",
+        "screen-2/4": "50vh",
+        "screen-3/4": "75vh",
+        "screen-1/5": "20vh",
+        "screen-2/5": "40vh",
+        "screen-3/5": "60vh",
+        "screen-4/5": "80vh",
+        "screen-1/6": "16.666667vh",
+      },
+    },
+  },
+  plugins: [],
+};
